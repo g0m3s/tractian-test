e7ca96cf5592db364ae08466c7ed5f54282cb3b1
g0m3s <gom3s.g@gmail.com>
2023-05-19 00:46:03 -0300

    feat: adding theme-color meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,19 @@
 import { theme } from "~/theme/base";
-import { ColorModeScript } from '@chakra-ui/react'
-import { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript } from "@chakra-ui/react";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#ECECEC" />
+      </Head>
       <body>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
 
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
